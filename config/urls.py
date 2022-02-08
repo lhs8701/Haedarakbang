2dcbf0705995abdc9cb6caf6084a3250ee2019aa
hansol <hansol8701@gmail.com>
2022-02-08 10:01:27 +0900

    test

--- config/urls.py
+++ config/urls.py
@@ -21,3 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('hdrb/', include('hdrb.urls'))
 ]
+# feature/reserv_create brnach !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

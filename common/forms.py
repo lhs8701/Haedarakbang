d01c6e0fe5aa18f7252fb15968baaa822412bef2
hansol <hansol8701@gmail.com>
2022-02-18 22:32:13 +0900

    login.html, signup.html css작업, 회원가입 이름란 추가

--- common/forms.py
+++ common/forms.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 
 class UserForm(UserCreationForm):
+    real_name = forms.CharField(label='이름')
     class Meta:
         model = User
-        fields = ("username","password1","password2")
+        fields = ("username","password1","password2","real_name")
